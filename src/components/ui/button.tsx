e7e25b2a1b665c3783c91902dca5a76aa26fe732
Ashirvad dubey <114186314+Ashirvaddubey@users.noreply.github.com>
2025-05-21 23:55:34 +0530

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+// designed to be customizable using utility classes and variants.
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
