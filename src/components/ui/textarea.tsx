43b712187cda49fdc1ef8961dec165704693b67c
Ashirvad dubey <114186314+Ashirvaddubey@users.noreply.github.com>
2025-05-22 00:25:52 +0530

    Update textarea.tsx

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,10 +1,8 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
@@ -22,3 +20,6 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
 Textarea.displayName = "Textarea"
 
 export { Textarea }
+
+
+
